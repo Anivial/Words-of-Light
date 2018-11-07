431193c4c3ae0754a2faa3fc8aa5a3326dc93514
Anivial <alien360@live.fr>
2018-11-07 23:19:57 +0100

    first commit

---
+++ android/src/com/anivial/wordsoflight/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.anivial.wordsoflight;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.anivial.wordsoflight.GameApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameApp(), config);
+	}
+}
